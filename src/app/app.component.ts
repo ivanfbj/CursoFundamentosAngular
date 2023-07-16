1ee9e5deb74392eb8ed7a22c529b1c45bc9576f3
ivanfbj <ivanfbj@gmail.com>
2023-07-16 13:35:08 -0500

    Clase 13 - *ngFor para arrays
    Crear modelo para definirlo como tipo a una variable.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';//Clase 13 - *ngFor para arrays
 
 /*División de responsabilidad
 Un componente de Angular se divide en tres archivos:
@@ -78,4 +79,52 @@ export class AppComponent {
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
   }
+
+  // Clase 12 - Uso de *ngFor
+  names: string[] = ['Nico', 'Juli', 'Santi']
+  newName = '';
+
+  addName() {
+    this.names.push(this.newName);
+    this.newName = '';
+  }
+
+  deleteName(index: number) {
+    this.names.splice(index, 1);
+  }
+
+  // Clase 13 - *ngFor para arrays
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
 }
---
+++ src/app/product.model.ts
@@ -0,0 +1,7 @@
+// Clase 13 - *ngFor para arrays
+export interface Product {
+  name: string;
+  price: number;
+  image: string;
+  category?: string; //Con el signo de interrogación se vuelve opcional el atributo
+}
