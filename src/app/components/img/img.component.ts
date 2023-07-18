6b483b7bbc401b2db740bb81f8d9f1c2d4156584
ivanfbj <ivanfbj@gmail.com>
2023-07-17 19:47:27 -0500

    Clase 3 - Uso de Inputs (parte 1)
    Se crea el atributo en el componente hijo y luego se le manda el valor a dicho atributo desde el componente padre.

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent {
+export class ImgComponent implements OnInit {
+
+  // Curso de Angular: Componentes y Servicios
+  // Clase 3 - Uso de Inputs
+  /* Para poder pasarle un valor desde el padre (app.component.html) al hijo (img.component.html)
+    se debe usar el decorador @Input para que el atributo de este componente pueda recibir la información.
+    por lo cual el atributo "img" se le cambiará el valor por el que se le envíe desde el padre (app.component.html)
+  */
+  @Input() img: string = 'valor init'
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
