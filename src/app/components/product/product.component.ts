4e2f1e3a877a91b34734b947148347097b909596
ivanfbj <ivanfbj@gmail.com>
2023-07-20 20:31:56 -0500

    Clase 5 - Componente para producto (Parte 2)
    Por buenas practicas se debe tipar el productos.
    Tipar el producto consiste en crear una clase de tipo modelo que permita definir una estructura para el producto, convirtiendola en una clase que se podrá instanciar desde otros componente.

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../../models/product.model'
 
 @Component({
   selector: 'app-product',
@@ -7,10 +8,17 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
 
-product = {
-  name: 'producto 1',
-  image: './assets/images/toy.jpg',
-  price: 100
-}
+  // Por buenas practicas se debe tipar el producto para saber que se va a esperar del producto.
+  // Por lo cual se crea una carpeta llamada "models" la cual va a contener un modelo particular para el producto.
+  /* Se realiza la importación del modelo creado y luego se indica de que tipo quiero que se mi variable "product"
+    quedaria de la siguiente manera "product: Product = {}".
+    Con esto ya quedó el producto de forma tipada.
+  */
+  product: Product = {
+    id: '1',
+    name: 'producto 1',
+    image: './assets/images/toy.jpg',
+    price: 100
+  }
 
 }
